7c09aaa532df09ffa9f3a19db78403f09fec965b
Ahmed Ejaz <chahmedejaz9050@gmail.com>
2024-02-18 18:20:49 +0500

    add routes -----route---------        ----controller actions---- GET	/students/new	       students#new POST /students	           students#create GET	/students/:id	       students#show GET	/students/edit/:id	   students#edit PATCH/PUT /students	       students#update DELETE /students	       students#destroy POST /students/search      students#search

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
+  resources :students do
+    collection do
+      post 'search'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "students#index"
 end
